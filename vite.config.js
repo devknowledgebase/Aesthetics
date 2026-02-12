406e65319e2428d078cf6ed9c06778987266f5b6
melt71 <blinkboy20073@gmail.com>
2026-02-12 13:48:16 +0530

    link

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://melt71.github.io/Aesthetics"
+  base: "https://devknowledgebase.github.io/Aesthetics"
 })
